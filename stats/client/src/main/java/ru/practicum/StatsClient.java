8463009a7afba1863c36934029e3ba97dd6385fe
EvgenyRodionov <globevgeny@yandex.ru>
2023-12-05 14:54:41 +0300

    feature: реализовать клиент

---
+++ stats/client/src/main/java/ru/practicum/StatsClient.java
@@ -0,0 +1,12 @@
+package ru.practicum;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface StatsClient {
+
+    EndpointHitDto addHit(HttpServletRequest request, String app);
+
+    List<StatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique);
+}
