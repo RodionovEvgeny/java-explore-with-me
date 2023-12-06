fe21e3b8304f21952573d27ad5651bd1d7148f61
EvgenyRodionov <globevgeny@yandex.ru>
2023-12-06 12:49:42 +0300

    feature: add docker

--- stats/server/src/main/java/ru/practicum/repository/EndpointHitRepository.java
+++ stats/server/src/main/java/ru/practicum/repository/EndpointHitRepository.java
@@ -9,42 +9,38 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface EndpointHitRepository extends JpaRepository<EndpointHit, Long> {
-    @Query("""
-            SELECT new ru.practicum.model.Stats(e.app, e.uri, COUNT(e.ip))
-            FROM EndpointHit AS e
-            WHERE e.timestamp >= ?1
-            AND e.timestamp <= ?2
-            GROUP BY e.app, e.uri
-            ORDER BY COUNT(e.ip) DESC""")
+    @Query("SELECT new ru.practicum.model.Stats(e.app, e.uri, COUNT(e.ip)) " +
+            "FROM EndpointHit AS e " +
+            "WHERE e.timestamp >= ?1 " +
+            "AND e.timestamp <= ?2 " +
+            "GROUP BY e.app, e.uri " +
+            "ORDER BY COUNT(e.ip) DESC")
     List<Stats> getStatsNoUrisNotUniqueIp(LocalDateTime start, LocalDateTime end);
 
-    @Query("""
-            SELECT new ru.practicum.model.Stats(e.app, e.uri, COUNT(DISTINCT e.ip))
-            FROM EndpointHit AS e
-            WHERE e.timestamp >= ?1
-            AND e.timestamp <= ?2
-            GROUP BY e.app, e.uri
-            ORDER BY COUNT(e.ip) DESC""")
+    @Query("SELECT new ru.practicum.model.Stats(e.app, e.uri, COUNT(DISTINCT e.ip)) " +
+            "FROM EndpointHit AS e " +
+            "WHERE e.timestamp >= ?1 " +
+            "AND e.timestamp <= ?2 " +
+            "GROUP BY e.app, e.uri " +
+            "ORDER BY COUNT(e.ip) DESC")
     List<Stats> getStatsNoUrisUniqueIp(LocalDateTime start, LocalDateTime end);
 
-    @Query("""
-            SELECT new ru.practicum.model.Stats(e.app, e.uri, COUNT(e.ip))
-            FROM EndpointHit AS e
-            WHERE e.timestamp >= ?1
-            AND e.timestamp <= ?2
-            AND e.uri in ?3
-            GROUP BY e.app, e.uri
-            ORDER BY COUNT(e.ip) DESC""")
+    @Query("SELECT new ru.practicum.model.Stats(e.app, e.uri, COUNT(e.ip)) " +
+            "FROM EndpointHit AS e " +
+            "WHERE e.timestamp >= ?1 " +
+            "AND e.timestamp <= ?2 " +
+            "AND e.uri in ?3 " +
+            "GROUP BY e.app, e.uri " +
+            "ORDER BY COUNT(e.ip) DESC")
     List<Stats> getStatsWithUrisNotUniqueIp(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("""
-            SELECT new ru.practicum.model.Stats(e.app, e.uri, COUNT(DISTINCT e.ip))
-            FROM EndpointHit AS e
-            WHERE e.timestamp >= ?1
-            AND e.timestamp <= ?2
-            AND e.uri in ?3
-            GROUP BY e.app, e.uri
-            ORDER BY COUNT(e.ip) DESC""")
+    @Query("SELECT new ru.practicum.model.Stats(e.app, e.uri, COUNT(DISTINCT e.ip)) " +
+            "FROM EndpointHit AS e " +
+            "WHERE e.timestamp >= ?1 " +
+            "AND e.timestamp <= ?2 " +
+            "AND e.uri in ?3 " +
+            "GROUP BY e.app, e.uri " +
+            "ORDER BY COUNT(e.ip) DESC")
     List<Stats> getStatsWithUrisUniqueIp(LocalDateTime start, LocalDateTime end, List<String> uris);
 
 }
